9b7730b1b31eeae516b178de0961b4718e2d8123
Felipe França <felipefrancafj@gmail.com>
2025-11-14 09:37:09 -0300

    x0.0.5

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,7 +4,7 @@ export interface ButtonProps {
   children: React.ReactNode;
 }
 
-export function Button({ children }: ButtonProps) {
+export default function Button({ children }: ButtonProps) {
   return (
     <button className="bg-background-default text-primary-main p-md p-lg rounded-md font-weight-medium border border-gray-100 cursor-pointer">
       {children}
